8122379f75d7adfbb1e152c86c58429f45cb78bf
Floris Cornielje <55357720+floris-sudo@users.noreply.github.com>
2022-12-02 15:45:15 +0100

    simplifieds and working
    remove all the bullcrapp and got it to work!

--- src/main.c
+++ src/main.c
@@ -1,36 +1,14 @@
 #include "main.h"
 
-void compareStrings(char* String1[], char* String2[]) {
-
-
-    char Characters[sizeof(String1)][2];
-
-    for (int c = 0; c < sizeof(String1); c++) {
-        Characters[c][0] = String1[c];
-    }
-    for (int c = 0; c < sizeof(String2); c++) {
-        Characters[c][1] = String2[c];
-    }
-
-    for (int c = 0; c < sizeof(Characters); c++) {
-        if (Characters[c][0] != Characters[c][1]) {
-            printf("The Strings are not the same!!\n\n");
-            printf("%s\n\n%s", *String1, *String2);
-            return;
-        }
-    }
-    printf("The Strings are the same!!");
-    printf("%s\n\n%s", *String1, *String2);
-    return;
-
-
-}
-
 int main(int argc, char *argv[]) {
 
     if (argc == 3) {
-        compareStrings(&argv[1], &argv[2]);
-        return 0;
+        if (strcmp(argv[1], argv[2]) == 0) {
+            printf("\n\nThe Strings are the same!\n\n");
+        }
+        else {
+            printf("\n\nThe strings are not the same\n\n%s\n%s\n\n", argv[1], argv[2]);
+        }
     }
     else {
         printf("\n\nThis tool takes 2 arguments!\n\nYou provided %d arguments!\n\nPlease try again.\n\n", argc -1);
